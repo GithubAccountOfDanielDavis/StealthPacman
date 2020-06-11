e446bb8d3ad05cae51758423abdf72e1491268e9
Dan Davis <emailaddressofdanieldavis@gmail.com>
2020-06-11 15:25:24 -0400

    Added FPS on screen

--- __main__.py
+++ __main__.py
@@ -17,14 +17,20 @@ def get_events():
         event_dict[event.type].append(event)
     return event_dict
 
+def display_fps(milliseconds, screen):
+    font = pg.font.SysFont('FreeMono, Monospace', 30, True)
+    text = font.render(f'FPS: {int(1.0 / (milliseconds / 1000.0))}', True, (0, 0, 0))
+    text.get_rect().left = 0
+    screen.blit(text, (218, 275))
+
 Renderable = typing.Callable[[pg.Surface], None]
-def game_loop(renderables: typing.Sequence[Renderable]):
+def game_loop(renderables: typing.Sequence[Renderable], fps=90):
     """Initialize pygame and run loop until it's over"""
     pg.init()
     pg.display.set_caption('Pacman')
     screen = pg.display.set_mode(grid.SCREEN_SIZE)
     clock = pg.time.Clock()
-    milliseconds = clock.tick(60)
+    milliseconds = clock.tick(fps)
 
     player = Player(pg.Vector2(13.5, 23))
     renderables.append(player.render)
@@ -37,8 +43,9 @@ def game_loop(renderables: typing.Sequence[Renderable]):
         player.update(milliseconds)
         for render_to in renderables:
             render_to(screen)
+        display_fps(milliseconds, screen)
         pg.display.update()
-        milliseconds = clock.tick(60)
+        milliseconds = clock.tick(fps)
     pg.quit()
     quit()
 
